a14a0e74fbd06d79f8b69484a1d07d87f8951808
dipubhandari <dipendrabhandari123456@gmail.com>
2023-06-01 02:49:46 -0700

    first commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from '../page.module.css'
+const Nav = () => {
+    return (
+        <>
+            <div className={styles.texts}>
+                <h1 className="heading">Better Ddesign for your digital products.</h1>
+                <p className="paragra">Turning your idea into reality.We bring together the teams from the global tech indeustry.</p>
+                <button>See Our Works</button>
+            </div>
+        </>
+    )
+}
+
+export default Nav
